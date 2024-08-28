73d79e82fc7f106c411a203b4a2c7367b56c2206
Jacint Simko <simkojacint@gmail.com>
2024-08-28 16:01:56 +0200

    config

--- src/Providers/GkkrServiceProvider.php
+++ src/Providers/GkkrServiceProvider.php
@@ -18,6 +18,43 @@ class GkkrServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->publishes([$this->absolutePackageDirectory('config/') => config_path()], 'config');
         $this->loadRoutesFrom(__DIR__.'/../routes/gkkr.php');
+    }
+
+    protected function getDirectoryContents($path): array
+    {
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS));
+
+        $files = [];
+        foreach ($iterator as $file) {
+            /**
+             * @var \FilesystemIterator $file
+             */
+            if ($file->isDir()) {
+                continue;
+            }
+
+            $files[] = $file->getPathname();
+        }
+
+        return $files;
+    }
+
+    protected function replaceFileContent(string $file, array|string $from, array|string $to): string|array
+    {
+        return str_replace($from, $to, file_get_contents($file));
+    }
+
+    /**
+     * Converting relative path to absolute path
+     *
+     * @param null $path
+     *
+     * @return string
+     */
+    private function absolutePackageDirectory($path = null): false|string
+    {
+        return realpath(__DIR__ . '/../' . $path);
     }
 }
